41455336ec804f804a518a811256b164d492914b
guanfei <guanfei@51credit.com>
2022-08-17 18:19:11 +0800

    [A0070]爬楼梯

---
+++ src/main/java/com/algorithm/dp/A0070ClimbStairs.java
@@ -0,0 +1,72 @@
+package com.algorithm.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 假设你正在爬楼梯。需要 n阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 示例 2：
+ *
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ *
+ * <a href="https://leetcode.cn/problems/climbing-stairs">algorithm</a>
+ */
+public class A0070ClimbStairs {
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public int climbStairs2(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        A0070ClimbStairs algorithm = new A0070ClimbStairs();
+        int ways = algorithm.climbStairs(30);
+//        int ways = algorithm.climbStairs(44, new HashMap<>()); //165580141
+        System.out.println(ways);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+}
