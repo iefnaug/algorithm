263b1eefb2e0b5144022fc30bdfe629f2d13314d
guanfei <guanfei@51credit.com>
2022-08-21 09:24:58 +0800

    [A0017]号码组合

---
+++ src/main/java/com/algorithm/bfsdfs/A0017PhoneNumber.java
@@ -0,0 +1,62 @@
+package com.algorithm.bfsdfs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ * <a href="https://leetcode.cn/problems/letter-combinations-of-a-phone-number">algorithm</a>
+ */
+public class A0017PhoneNumber {
+
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuilder());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuilder combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String digits = "234";
+        A0017PhoneNumber algorithm = new A0017PhoneNumber();
+        List<String> result = algorithm.letterCombinations(digits);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+
+
+}
