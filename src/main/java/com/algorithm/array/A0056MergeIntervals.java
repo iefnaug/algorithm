052aa18497723922c43f90771e972ed4856d6c9c
guanfei <guanfei@51credit.com>
2023-04-23 12:47:44 +0800

    [leetcode]56合并区间

---
+++ src/main/java/com/algorithm/array/A0056MergeIntervals.java
@@ -0,0 +1,61 @@
+package com.algorithm.array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ *
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ *
+ * @author GF
+ * @since 2023/4/23
+ */
+public class A0056MergeIntervals {
+
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(v -> v[0]));
+        int[][] result = new int[intervals.length][2];
+        int index = 0;
+        int[] current = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (current[1] < intervals[i][0]) {
+                result[index++] = current;
+                current = intervals[i];
+            } else {
+                current[1] = Math.max(intervals[i][1], current[1]);
+            }
+        }
+        result[index] = current;
+        int[][] target = new int[index+1][2];
+        System.arraycopy(result, 0, target, 0, index + 1);
+        return target;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        A0056MergeIntervals algorithm = new A0056MergeIntervals();
+        int[][] merge = algorithm.merge(intervals);
+        System.out.println();
+    }
+
+}
