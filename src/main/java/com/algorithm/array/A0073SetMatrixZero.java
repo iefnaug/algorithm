47f584fd39acfd5f038269495369c4aeef701832
guanfei <guanfei@51credit.com>
2023-04-27 00:09:38 +0800

    [lc]73矩阵置零

---
+++ src/main/java/com/algorithm/array/A0073SetMatrixZero.java
@@ -0,0 +1,79 @@
+package com.algorithm.array;
+
+/**
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ *
+ * 输入：
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * 输出：
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * 示例 2：
+ *
+ * 输入：
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * 输出：
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ *
+ * @author GF
+ * @since 2023/4/26
+ */
+public class A0073SetMatrixZero {
+
+
+    public void setZeroes(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}
+        };
+
+        A0073SetMatrixZero algorithm = new A0073SetMatrixZero();
+        algorithm.setZeroes(matrix);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
