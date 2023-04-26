5dbad11704eae455c11c381002fce61adb98c952
guanfei <guanfei@51credit.com>
2023-04-26 22:13:17 +0800

    [lc]48旋转矩阵

---
+++ src/main/java/com/algorithm/array/A0048RotateImage.java
@@ -0,0 +1,65 @@
+package com.algorithm.array;
+
+/**
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ *
+ * 不占用额外内存空间能否做到？
+ *
+ *
+ * 示例 1:
+ *
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * 示例 2:
+ *
+ * 给定 matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ *
+ * @author GF
+ * @since 2023/4/26
+ */
+public class A0048RotateImage {
+
+    /**
+     * matrix[row][col] = matrix[col][n-row-1]
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] tmp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tmp[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = tmp[i][j];
+            }
+        }
+    }
+
+}
