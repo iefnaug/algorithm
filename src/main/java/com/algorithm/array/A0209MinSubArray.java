e0d136ab273b0f8727f15122e39c4cbc3305f290
guanfei <guanfei@51credit.com>
2023-05-11 15:07:38 +0800

    [lc-209]长度最小的子数组

---
+++ src/main/java/com/algorithm/array/A0209MinSubArray.java
@@ -0,0 +1,72 @@
+package com.algorithm.array;
+
+/**
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ *
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 示例 2：
+ *
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * 1 <= target <= 10^9
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^5
+ *
+ *
+ * 进阶：
+ *
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ * @author GF
+ * @since 2023/5/11
+ */
+public class A0209MinSubArray {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = Integer.MAX_VALUE;
+        int slow = 0, fast = 0;
+        int sum = nums[0];
+        while (fast < nums.length) {
+            if (sum < target) {
+                fast++;
+                if (fast == nums.length) {
+                    break;
+                }
+                sum += nums[fast];
+            } else {
+                if (slow == fast) {
+                    return 1;
+                }
+                len = Math.min(len, fast - slow + 1);
+                sum -= nums[slow];
+                slow++;
+            }
+        }
+        return len == Integer.MAX_VALUE ? 0 : len;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {
+                2,3,1,2,4,3
+        };
+        A0209MinSubArray algorithm = new A0209MinSubArray();
+        int len = algorithm.minSubArrayLen(7, nums);
+        System.out.println(len);
+    }
+
+}
