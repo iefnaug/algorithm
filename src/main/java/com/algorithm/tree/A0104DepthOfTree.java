dc97690c4c59fdc56a1e82cda42408d5c25c9621
guanfei <guanfei@51credit.com>
2023-06-21 23:44:34 +0800

    [lc-104]二叉树最大深度

---
+++ src/main/java/com/algorithm/tree/A0104DepthOfTree.java
@@ -0,0 +1,57 @@
+package com.algorithm.tree;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * <p>
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3
+ *
+ * @author GF
+ * @since 2023/6/21
+ */
+public class A0104DepthOfTree {
+
+
+    public int maxDepth(TreeNode root) {
+        return depth(root);
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(depth(node.left), depth(node.right));
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
