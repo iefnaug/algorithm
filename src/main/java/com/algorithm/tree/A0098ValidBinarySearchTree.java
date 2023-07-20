6221fdcd87552da12300bd9d1a02a018702fa2a3
guanfei <guanfei@51credit.com>
2023-07-20 14:35:50 +0800

    [lc-98]判断二叉搜索树

---
+++ src/main/java/com/algorithm/tree/A0098ValidBinarySearchTree.java
@@ -0,0 +1,44 @@
+package com.algorithm.tree;
+
+/**
+ *给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * @author GF
+ * @since 2023/7/19
+ */
+public class A0098ValidBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+
+    private boolean valid(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return valid(node.left, lower, node.val) && valid(node.right, node.val, upper);
+    }
+
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+}
