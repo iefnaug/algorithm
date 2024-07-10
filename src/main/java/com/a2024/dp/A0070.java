8a898a9399232ad2aa5abdbf52a3c3148c8c9c2c
iefnaug <guanfei1993@gmail.com>
2024-07-10 16:56:45 +0800

    爬楼梯

---
+++ src/main/java/com/a2024/dp/A0070.java
@@ -0,0 +1,44 @@
+package com.a2024.dp;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ */
+public class A0070 {
+
+    /**
+     * f(n) = f(n-1) + f(n-2)
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        int i = new A0070().climbStairs2(6);
+        System.out.println(i);
+    }
+
+}
