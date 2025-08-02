b928463510e76b27ae43efecf1069e11e50dc294
iefnaug <guanfei1993@gmail.com>
2025-08-02 21:32:06 +0800

    [lc-238] 除自身以外数组的乘积

---
+++ src/main/java/com/a2025/str/A0238Multiple.java
@@ -0,0 +1,78 @@
+package com.a2025.str;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/*
+给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+
+题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+
+请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+
+
+
+示例 1:
+
+输入: nums = [1,2,3,4]
+输出: [24,12,8,6]
+示例 2:
+
+输入: nums = [-1,1,0,-3,3]
+输出: [0,0,9,0,0]
+
+
+提示：
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+输入 保证 数组 answer[i] 在  32 位 整数范围内
+
+
+进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组 不被视为 额外空间。）
+ */
+public class A0238Multiple {
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] l2r = new int[nums.length], r2l = new int[nums.length], answer = new int[nums.length];
+        l2r[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            l2r[i] = l2r[i-1] * nums[i-1];
+        }
+        r2l[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            r2l[i] = r2l[i+1] * nums[i+1];
+        }
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = l2r[i] * r2l[i];
+        }
+        return answer;
+    }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int[] answer = new int[nums.length];
+        int multi = 1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = multi;
+            multi *= nums[i];
+        }
+        multi = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * multi;
+            multi *= nums[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {
+            1,2,3,4
+        };
+        A0238Multiple algo = new A0238Multiple();
+        int[] answers = algo.productExceptSelf2(nums);
+        for (int answer : answers) {
+            System.out.print(answer + ",");
+        }
+    }
+
+}
